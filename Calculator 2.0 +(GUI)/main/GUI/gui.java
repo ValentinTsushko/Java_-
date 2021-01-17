7daece1ab704ef232471533ae7fc129582ec5a25
Valentin_Tsushko <35625439+vladwolf@users.noreply.github.com>
2021-01-17 19:23:46 +0200

    Update gui.java

--- Calculator 2.0 +(GUI)/main/GUI/gui.java
+++ Calculator 2.0 +(GUI)/main/GUI/gui.java
@@ -10,40 +10,51 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class gui implements ActionListener{
-	Scann sc = new Scann();
-	
+	JTextField field = new JTextField(100);
+	JTextArea text;
 
-	
-	public void actionPerfomed(ActionEvent event){
+	public void actionPerformed(ActionEvent e){
+		Scann sc = new Scann();
 		System.out.println("OK");
-		//String tmp = field.getText();
-		//sc.takeLine(tmp);
-	}
+		String tmp = field.getText();
+		sc.takeLine(tmp);
+		double t = sc.view_oper(sc);
+		tmp = Double.toString(t);
+
+		text.append("\n Ok \n");
+		text.append(tmp);
+	}	
 	
 	public void go(){
 		JFrame frame = new JFrame();
+		//  panelka
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.darkGray);
+		
 		//  text pole
-		JTextField field = new JTextField(100);
 		field.setText("Your's virajenie");
 		System.out.print(field.getText());
 		field.selectAll();
-		//field.requestFocus();
-		frame.getContentPane().add(BorderLayout.NORTH, field);
+		text = new JTextArea(10, 20);
+		text.setLineWrap(true); // perenos text
+		panel.add(text);
 		
-		//JTextArea text = new JTextArea(10, 20);
-		//frame.getContentPane().add(BorderLayout.CENTER, text);
-		
+		//  vivod
+		JScrollPane scroller = new JScrollPane(text); //  peredaem obl. text
+		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		panel.add(scroller);
 		
 		//  knopka
 		JButton button = new JButton("Do it!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		button.setSize(100, 10);
 		
+		//  otobrajenie
 		button.addActionListener(this);
+		frame.getContentPane().add(BorderLayout.CENTER, panel);
+		frame.getContentPane().add(BorderLayout.NORTH, field);
 		frame.getContentPane().add(BorderLayout.SOUTH, button);
-		
-		//frame.getContentPane().add(button);
-		//frame.getContentPane().add(field);
 		frame.setSize(300, 300);
 		frame.setVisible(true);
 	}
