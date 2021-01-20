447eb21b5334a104c4e1e2d52981c1bec46031cd
Valentin_Tsushko <35625439+vladwolf@users.noreply.github.com>
2021-01-20 13:03:38 +0200

    Create last.java

---
+++ Calculator 2.1 + (GUI) + (Graphical calc)/main/last/last.java
@@ -0,0 +1,14 @@
+package last;
+
+import java.util.ArrayDeque;
+
+public class last{
+
+	ArrayDeque<Double> last = new ArrayDeque<Double>();
+	
+	public void add_last(double x){
+		last.addLast(x);
+	}
+	
+
+}
