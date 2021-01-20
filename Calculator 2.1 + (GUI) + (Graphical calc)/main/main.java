17e57e3c035f10dbc0329a85c7647c54a4603773
Valentin_Tsushko <35625439+vladwolf@users.noreply.github.com>
2021-01-20 13:00:12 +0200

    Create main.java

---
+++ Calculator 2.1 + (GUI) + (Graphical calc)/main/main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import plus.*;
+import last.*;
+import minus.*;
+import Scanner.*;
+import GUI.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class main{
+	public static void main(String[] args) {
+		gui Gui = new gui();
+		Scann sc = new Scann();
+		Gui.go();
+    }
+}
